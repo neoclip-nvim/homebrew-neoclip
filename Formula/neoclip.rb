d08ed6cb76c8e13eec9685790a9d408f1c3321cd
Slava <visla.vvi@gmail.com>
2025-03-07 17:48:39 +0500

    fixup `brew audit` errors

--- Formula/neoclip.rb
+++ Formula/neoclip.rb
@@ -1,23 +1,24 @@
 class Neoclip < Formula
-  desc "Neovim multi-platform clipboard provider."
+  desc "Neovim multi-platform clipboard provider"
   homepage "https://github.com/matveyt/neoclip"
-  license "Unlicense"
-  head "https://github.com/matveyt/neoclip.git"
-  #
-  url "https://github.com/matveyt/neoclip/archive/adfb1430c7308f0b3036f89b037509b3d0b4610a.zip"
+  url "https://github.com/matveyt/neoclip/archive/adfb1430c7308f0b3036f89b037509b3d0b4610a.tar.gz"
   version "2024.08.23.1"
+  sha256 "944b6b00a0e5c3d497d09d25c7d4c75bd04ddfcba2e85abf6309c94f3434591b"
+  license "Unlicense"
+  head "https://github.com/matveyt/neoclip.git", branch: "master"
 
   keg_only "it is a neovim plugin"
 
   depends_on "cmake" => :build
   depends_on "extra-cmake-modules" => :build
   depends_on "libffi" => :build
-  depends_on "pkg-config" => :build
   depends_on "luajit" => :build
+  depends_on "pkg-config" => :build
+  depends_on "nvim" => :test
   on_linux do
+    depends_on "libx11" => :build
     depends_on "wayland" => :build
     depends_on "wayland-protocols" => :build # it may be redundant
-    depends_on "libx11" => :build
   end
 
   def install
@@ -28,15 +29,13 @@ class Neoclip < Formula
     prefix.install "lua"
   end
 
-  depends_on "nvim" => :test
-
   # Test requires either x11 or wayoand or macosx
   test do
-    health = 'health.log'
+    health = "health.log"
     system "nvim", "--headless",
       # set up
-      "+set\ rtp+=#{prefix}",
-      "+lua\ require('neoclip'):setup()",
+      "+set rtp+=#{prefix}",
+      "+lua require('neoclip'):setup()",
       # execute
       "'+checkhealth neoclip'", "+w!#{health}", "+qa"
 
@@ -48,6 +47,6 @@ class Neoclip < Formula
     system "grep", "-v", "FAIL", health
 
     # cleanup
-    system "rm", health
+    rm health
   end
 end
